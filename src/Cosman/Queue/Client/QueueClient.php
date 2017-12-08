568deb9e60d321bc59abe557610fbf81f533a5b7
Agbitor Cosman <agbitorcosman@gmail.com>
2017-12-08 00:24:39 +0000

    Added implementation for projects method

--- src/Cosman/Queue/Client/QueueClient.php
+++ src/Cosman/Queue/Client/QueueClient.php
@@ -3,6 +3,8 @@ declare(strict_types = 1);
 namespace Cosman\Queue\Client;
 
 use Cosman\Queue\Client\Connection\QueueConnection;
+use Cosman\Queue\Client\Http\Request\ProjectRequest;
+use function GuzzleHttp\Promise\settle;
 
 /**
  *
@@ -20,14 +22,28 @@ class QueueClient
 
     /**
      *
+     * @var ProjectRequest
+     */
+    protected $projectRequest;
+
+    /**
+     *
      * @param QueueConnection $connection
      */
     public function __construct(QueueConnection $connection)
     {
         $this->connection = $connection;
-    }
-    
-    public function projects() {
         
+        $this->projectRequest = new ProjectRequest($connection);
+    }
+
+    /**
+     * Returns request service for querying projects on the server
+     *
+     * @return ProjectRequest
+     */
+    public function projects(): ProjectRequest
+    {
+        return $this->projectRequest;
     }
 }
